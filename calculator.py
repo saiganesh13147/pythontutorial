11da7060680bd01fa4d023bb54ee1085e4cd4e13
sai ganesh <saiganesh@sais-MBP.ht.home>
2026-08-10 23:16:23 -0400

    Update addition message

--- calculator.py
+++ calculator.py
@@ -18,7 +18,7 @@ def divide(a, b):
     return a / b
 
 
-print("Addition:", add(10, 5))
+print("Sum:", add(10, 5))
 print("Subtraction:", subtract(10, 5))
 print("Multiplication:", multiply(10, 5))
 print("Division:", divide(10, 5))
